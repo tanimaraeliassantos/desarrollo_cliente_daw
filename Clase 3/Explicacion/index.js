46f20e19cf5b44fc38229f6c9f4596433ff126e7
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-09-26 12:36:27 +0200

    Update index.js
    Crear funcion para identificar clics pares o impares

--- Clase 3/Explicacion/index.js
+++ Clase 3/Explicacion/index.js
@@ -1,3 +1,18 @@
 function mostrarMensajeExterno() {
 	alert('Esta es la mejor opción documento JS externo');
 }
+
+let n = 1;
+
+function mostrarMensajeExterno2() {
+	alert('Esta es la mejor opción documento JS externo DOS ' + n);
+	n++;
+}
+
+function clickParImpar() {
+	if (n % 2 == 0) {
+		alert('CLICK PAR: ' + n);
+	} else {
+		alert('CLICK IMPAR: ' + n);
+	}
+}
