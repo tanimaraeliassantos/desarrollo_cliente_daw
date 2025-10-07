fa9c3541064e9b661fcbf391d9d8ab216987f372
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-07 14:41:01 +0200

    Update ejemploFetch.js
    Seleccionando datos de dentro del JSON

--- Clase 6/ejemploFetch.js
+++ Clase 6/ejemploFetch.js
@@ -2,4 +2,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
 	.then((response) => response.json())
 	.then((users) => {
 		console.log(users);
+		users.forEach((user) => {
+			console.log(user.name);
+		});
 	});
