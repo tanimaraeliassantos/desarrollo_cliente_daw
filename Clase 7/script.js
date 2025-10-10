e3f5225c586f7e2ecfb058415149519e8386b963
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-10 22:00:04 +0200

    Update script.js

--- Clase 7/script.js
+++ Clase 7/script.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', function (addEventListener) {
+document.addEventListener(('DOMContentLoaded', () => {
 	const form = document.getElementById('miFormulario');
-});
+    const nombre = document.getElementById('nombre');
+    const edad = document.getElementById('edad');
+    const email = document.getElementById('email');
+    const telefono = document.getElementById('telefono');
+
+    form.addEventListener("submit", () => {
+        if(nombre.value.length < 3) {
+            alert("El nombre tiene que tener al menos 3 caracteres")
+            return;
+        }
+    })
+  
+}))
