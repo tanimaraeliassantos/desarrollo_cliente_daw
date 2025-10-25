e51389aaf68461880319addd321939c943113dcc
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-25 13:12:42 +0200

    Update app.ts

--- Clase 11/app/src/app/app.ts
+++ Clase 11/app/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommsPadre } from "./components/comms-padre/comms-padre";
+import { CommsPadre } from './components/comms-padre/comms-padre';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommsPadre],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('app');
