bc672e446ad7876fbdc13e7b5ba703e0aeefa8c6
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-25 13:09:11 +0200

    Update app.ts

--- Clase 11/app/src/app/app.ts
+++ Clase 11/app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommsPadre } from "./components/comms-padre/comms-padre";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommsPadre],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
