1369e6226c52d3bbd0039a88a61b15e7a2da2ee7
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-11-07 11:27:36 +0100

    Creado proyecto nuevo con Angular

---
+++ Clase 13/captura-eventos/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('captura-eventos');
+  //USUARIO: capturar los clicks
+  clicks = signal(0)
+  hacerClick(){
+    this.clicks.update(v => v + 1)
+  }
+}
