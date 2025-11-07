bfcbf89679069a2783c1b5879e23f37a171d915d
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-11-07 11:37:53 +0100

    Update app.ts

--- Clase 13/captura-eventos/src/app/app.ts
+++ Clase 13/captura-eventos/src/app/app.ts
@@ -1,17 +1,21 @@
 import { Component, signal } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('captura-eventos');
   //USUARIO: capturar los clicks
-  clicks = signal(0)
-  hacerClick(){
-    this.clicks.update(v => v + 1)
+  clicks = signal(0);
+  hacerClick() {
+    this.clicks.update((v) => v + 1);
+  }
+
+  mensaje = signal('');
+  saludarA(nombre: string) {
+    this.mensaje.set(`Hola, ${nombre}`);
   }
 }
