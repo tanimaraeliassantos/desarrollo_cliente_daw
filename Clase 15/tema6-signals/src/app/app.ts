bf193ca18ba39a8031628b5ea3fa1454d84107de
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-12-12 14:45:58 +0100

    Incluido componente flujo-switch

--- Clase 15/tema6-signals/src/app/app.ts
+++ Clase 15/tema6-signals/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { FlujoIf } from "./components/flujo-if/flujo-if";
-import { FlujoFor } from "./components/flujo-for/flujo-for";
+import { FlujoIf } from './components/flujo-if/flujo-if';
+import { FlujoFor } from './components/flujo-for/flujo-for';
+import { FlujoSwitch } from './components/flujo-switch/flujo-switch';
 
 @Component({
   selector: 'app-root',
-  imports: [FlujoIf, FlujoFor],
+  imports: [FlujoIf, FlujoFor, FlujoSwitch],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('tema6-signals');
