b104fc1163f65bd1967a7e3f08c8eea7b7cca713
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-15 16:03:37 +0100

    Update app con formulario reactivo

--- Clase 16/tema7-angular/src/app/app.ts
+++ Clase 16/tema7-angular/src/app/app.ts
@@ -2,10 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListaColoresComponent } from './components/lista-colores/lista-colores';
 import { FormTemplateComponent } from './components/form-template/form-template';
+import { FormReactivoComponent } from './components/form-reactivo/form-reactivo';
 
 @Component({
   selector: 'app-root',
-  imports: [ListaColoresComponent, FormTemplateComponent],
+  imports: [
+    ListaColoresComponent,
+    FormTemplateComponent,
+    FormTemplateComponent,
+    FormReactivoComponent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
