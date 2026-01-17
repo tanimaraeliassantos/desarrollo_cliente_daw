174f4fd7e9f06d90bcb7fcdbc2c722b1d614abdc
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-17 13:29:13 +0100

    Cambio en componentes

--- Clase 16/tema7-angular/src/app/components/form-validado/form-validado.ts
+++ Clase 16/tema7-angular/src/app/components/form-validado/form-validado.ts
@@ -1,16 +1,58 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form-validado',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-validado.html',
   styleUrl: './form-validado.css',
 })
-export class FormValidado {
+export class FormValidadoComponent {
   form = new FormGroup({
-    nombre: new FormControl('',
-    [Validators.required, Validators.pattern])
-  })
+    nombre: new FormControl('', [Validators.required, Validators.pattern('[\\w\\-\\s\\/]+')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    edad: new FormControl('', [Validators.required, this.rangoEdadValidator(18, 65)]),
+  });
 
+  get nombre() {
+    return this.form.controls.nombre;
+  }
+  get email() {
+    return this.form.controls.email;
+  }
+  get edad() {
+    return this.form.controls.edad;
+  }
+  rangoEdadValidator(min: number, max: number) {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const raw = control.value;
+      if (raw === null || raw === '') return null; // lo gestiona required
+      const edad = Number(raw);
+
+      if (Number.isNaN(edad)) {
+        return { rangoEdad: { message: 'La edad debe ser un número' } };
+      }
+
+      if (edad >= min && edad <= max) {
+        return null;
+      } else {
+        return { rangoEdad: { message: `La edad debe estar entre ${min} y ${max}` } };
+      }
+    };
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Formulario válido:', this.form.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
