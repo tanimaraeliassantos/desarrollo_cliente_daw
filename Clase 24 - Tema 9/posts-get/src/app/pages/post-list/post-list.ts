f5eccbfde7eadd09eae66f9bbb9bb8e4a55d4080
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-02-05 15:10:36 +0100

    Añadida funcionalidad listar contenido

--- Clase 24 - Tema 9/posts-get/src/app/pages/post-list/post-list.ts
+++ Clase 24 - Tema 9/posts-get/src/app/pages/post-list/post-list.ts
@@ -1,20 +1,70 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Observable } from 'rxjs';
 import { Post } from '../../models/post';
 import { PostsService } from '../../services/posts';
 import { AsyncPipe } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Observable, combineLatest, map, startWith } from 'rxjs';
 
 @Component({
-  selector: 'app-posts-list',
-  imports: [RouterLink, AsyncPipe],
+  selector: 'app-post-list',
+  imports: [AsyncPipe, RouterLink, ReactiveFormsModule],
   templateUrl: './post-list.html',
   styleUrl: './post-list.css',
 })
 export class PostListComponent {
   posts$!: Observable<Post[]>;
 
+  searchControl = new FormControl('', { nonNullable: true });
+  userIdControl = new FormControl<number | null>(null);
+  sortControl = new FormControl<'id-asc' | 'id-desc' | 'title-asc' | 'title-desc'>('id-asc', {
+    nonNullable: true,
+  });
+
+  filteredPosts$!: Observable<Post[]>;
+
   constructor(private postsService: PostsService) {
+    //  Inicialización segura
     this.posts$ = this.postsService.getAll();
+
+    this.filteredPosts$ = combineLatest([
+      this.posts$,
+      this.searchControl.valueChanges.pipe(startWith(this.searchControl.value)),
+      this.userIdControl.valueChanges.pipe(startWith(this.userIdControl.value)),
+      this.sortControl.valueChanges.pipe(startWith(this.sortControl.value)),
+    ]).pipe(
+      map(([posts, term, userId, sort]) => {
+        const q = term.trim().toLowerCase();
+
+        const filtered = posts.filter((p) => {
+          const matchesText = !q || (p.title + ' ' + p.body).toLowerCase().includes(q);
+
+          const matchesUser = userId === null || p.userId === userId;
+
+          return matchesText && matchesUser;
+        });
+
+        const sorted = [...filtered];
+        sorted.sort((a, b) => {
+          switch (sort) {
+            case 'title-asc':
+              return a.title.localeCompare(b.title);
+            case 'title-desc':
+              return b.title.localeCompare(a.title);
+            case 'id-desc':
+              return b.id - a.id;
+            default:
+              return a.id - b.id;
+          }
+        });
+
+        return sorted;
+      }),
+    );
+  }
+  clear() {
+    this.searchControl.setValue('');
+    this.userIdControl.setValue(null);
+    this.sortControl.setValue('id-asc');
   }
 }
