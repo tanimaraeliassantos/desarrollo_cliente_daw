ded5d5b02d5748f43b4f83111d0c6b407d8fb06a
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-02-05 15:10:08 +0100

    Añadida funcionalidad editar contenido

--- Clase 24 - Tema 9/posts-get/src/app/pages/post-form/post-form.ts
+++ Clase 24 - Tema 9/posts-get/src/app/pages/post-form/post-form.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { PostsService } from '../../services/posts';
 import { Post } from '../../models/post';
+import { of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-post-form',
@@ -11,26 +12,53 @@ import { Post } from '../../models/post';
   styleUrl: './post-form.css',
 })
 export class PostFormComponent {
+  postId?: number;
+
   form = new FormGroup({
     title: new FormControl('', { nonNullable: true }),
     body: new FormControl('', { nonNullable: true }),
   });
   constructor(
+    private route: ActivatedRoute,
     private postsService: PostsService,
     private router: Router,
-  ) {}
+  ) {
+    // Si hay :id => modo editar => cargamos el post y rellenamos el form
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          const id = params.get('id');
+          if (!id) return of(null);
+          this.postId = Number(id);
+          return this.postsService.getById(this.postId);
+        }),
+      )
+      .subscribe((post) => {
+        if (post) {
+          this.form.patchValue({
+            title: post.title,
+            body: post.body,
+          });
+        }
+      });
+  }
 
   save() {
     const payload: Post = {
       userId: 1,
       title: this.form.controls.title.value,
       body: this.form.controls.body.value,
-      id: 0, // no es necesario, pero no molesta
+      id: this.postId ?? 0, // no es necesario, pero no molesta
     };
 
-    this.postsService.create(payload).subscribe({
-      next: (created) => this.router.navigate(['/posts', created.id ?? 1]),
-      error: (e) => console.error('Error creando:', e),
+    // si hay postId => PUT, si no => POST
+    const request$ = this.postId
+      ? this.postsService.update(this.postId, payload)
+      : this.postsService.create(payload);
+
+    request$.subscribe({
+      next: (res) => this.router.navigate(['/posts', res.id ?? this.postId ?? 1]),
+      error: (e) => console.error('Error guardando:', e),
     });
   }
 }
