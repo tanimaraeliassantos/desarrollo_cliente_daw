51f88cad2af94e4fcd89e6fb62d7bf91f6ca6db2
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-02-17 15:50:02 +0100

    Creadas páginas home y new

---
+++ 2-prueba-trimestral-tanimara-elias-santos-daw/proyecto-series/src/app/pages/new/new.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new',
+  imports: [],
+  templateUrl: './new.html',
+  styleUrl: './new.css',
+})
+export class New {
+
+}
