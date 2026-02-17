25eb543565811aae5770b0937f98e42881e18551
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-02-17 16:04:15 +0100

    Modificado home

--- 2-prueba-trimestral-tanimara-elias-santos-daw/proyecto-series/src/app/pages/home/home.ts
+++ 2-prueba-trimestral-tanimara-elias-santos-daw/proyecto-series/src/app/pages/home/home.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { SeriesService } from '../../services/series';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  series: any[] = [];
+
+  constructor(private seriesService: SeriesService) {}
+
+  ngOnInit(): void {
+    this.seriesService.getAll().subscribe((data: any) => {
+      this.series = data.data ?? data;
+    });
+  }
+}
