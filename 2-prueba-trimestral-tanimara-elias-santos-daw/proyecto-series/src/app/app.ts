c85b2d08acf36f79f69772a2892d81217b1dbbe7
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-02-17 15:42:56 +0100

    Componente footer creado

--- 2-prueba-trimestral-tanimara-elias-santos-daw/proyecto-series/src/app/app.ts
+++ 2-prueba-trimestral-tanimara-elias-santos-daw/proyecto-series/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header';
+import { FooterComponent } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
